d64c023e86859154edfb6134507f420a6f7e9760
Rafael Nascimento <rafael.doug@gmail.com>
2020-03-25 01:35:38 -0300

    adicao de comentarios

--- classificacao/template_credit_data.py
+++ classificacao/template_credit_data.py
@@ -2,19 +2,20 @@ import pandas as pd
 import numpy as np
 
 base = pd.read_csv('credit_data.csv')
-base.loc[base.age < 0, 'age'] = 40.92
+base.loc[base.age < 0, 'age'] = 40.92 # mean das idades validas (!NaN e positivos)
                
-previsores = base.iloc[:, 1:4].values
-classe = base.iloc[:, 4].values
+previsores = base.iloc[:, 1:4].values # variavel recebe as colunas dos previsores
+classe = base.iloc[:, 4].values # variavel a coluna da classe
 
 from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean') # substitui os valores NaN pela media
 imputer = imputer.fit(previsores[:, 1:4])
 previsores[:, 1:4] = imputer.transform(previsores[:, 1:4])
 
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
-previsores = scaler.fit_transform(previsores)
+previsores = scaler.fit_transform(previsores) # escalonamento dos previsores
 
+# split da base de dados para treinamento e teste
 from sklearn.model_selection import train_test_split
 previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25, random_state=0)
